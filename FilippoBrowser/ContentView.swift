6a69905aa67ac58373ff164e86bbcf70c1c8f29b
alessaba <alessandrosaba023@gmail.com>
2021-06-10 10:59:25 +0200

    New bordered GoTo buttons Code Cleanup

--- FilippoBrowser/ContentView.swift
+++ FilippoBrowser/ContentView.swift
@@ -31,9 +31,7 @@ struct Browser : View {
 	var body: some View {
 		NavigationView {
 			properDirectoryBrowser(for: FSItem(path: path))
-			//DirectoryGridBrowser(directory: )
-                .navigationBarTitle(
-					Text("File Browser"), displayMode: .inline)
+			.navigationBarTitle(Text("File Browser"), displayMode: .inline)
             .navigationBarItems(
                 
                 leading:
@@ -180,7 +178,7 @@ struct DirectoryListBrowser : View {
 		
 }
 
-
+#warning("Fix Dark cells in Light mode (gridView)")
 // MARK: Directory Grid Viewer
 // This is the directory browser, it shows files and subdirectories of a folder in grid style
 struct DirectoryGridBrowser : View {
@@ -191,14 +189,6 @@ struct DirectoryGridBrowser : View {
 	var body: some View {
 		ScrollView {
 			VStack{
-				/*HStack{
-					Image(systemName: "magnifyingglass")
-					TextField("Search..." , text: $searchText)
-				}
-				.padding(.all, 15)
-				.background(Color.init(.displayP3, white: 0.10, opacity: 1.0))
-				.padding(.vertical, 30)*/
-				
 				LazyVGrid(columns: Array(repeating: .init(.adaptive(minimum: 150, maximum: 150)), count: 3) as [GridItem]){
 					
 					let subelements = directory.subelements.filter{
@@ -279,10 +269,11 @@ struct DirectoryGridBrowser : View {
 	}
 }
 
+#warning("Must set a @State Property on the keys variable so when the variable is modified, the list is redrawn")
 // MARK: Go To View
 struct gotoView : View {
 	@State var path : String = "/"
-	#warning("Must set a @State Property on the keys variable so when the variable is modified, the list is redrawn")
+	
 	let userDefaultsKeys = UserDefaults.standard.dictionaryRepresentation().keys.filter{
 		$0.starts(with: "FB_")
 	}
@@ -293,96 +284,50 @@ struct gotoView : View {
             
 			TextField("Path", text: $path)
                 .padding(.all)
-				.border(Color.secondary, width: 4)
-				.cornerRadius(5)
-				.padding(.all)
+				.textFieldStyle(.roundedBorder)
 				
-			
 			NavigationLink(destination: properView(for: FSItem(path: path))){
 				Text("Go")
 					.foregroundColor(.primary)
 					.bold()
 					.padding()
 					.background(Color.green)
-					.cornerRadius(15)
+					.cornerRadius(25)
 					.safeHover()
 			}
 			
-			Spacer()
+			Spacer(minLength: 25)
 			
 			ScrollView {
-				Spacer()
+				BookmarkItem(name: "App Group ⌚️", path: appGroup_directory)
 				
-				NavigationLink(destination: properView(for: FSItem(path: appGroup_directory))){
-					Text("App Group ⌚️")
-						.foregroundColor(.primary)
-						.bold()
-						.padding()
-						.background(Color.blue)
-						.cornerRadius(15)
-						.padding(.horizontal, 10)
-						.safeHover()
-				}
 				#if os(iOS) || os(watchOS)
-				Spacer()
-				
-				NavigationLink(destination: properView(for: FSItem(path: "/var/mobile/Media/"))){
-					Text("Media 🖥")
-						.foregroundColor(.primary)
-						.bold()
-						.padding()
-						.background(Color.blue)
-						.cornerRadius(15)
-						.padding(.horizontal, 10)
-						.safeHover()
-				}
-				Spacer()
-				
-				NavigationLink(destination: properView(for: FSItem(path: documents_directory))){
-					Text("Documents 🗂")
-						.foregroundColor(.primary)
-						.bold()
-						.padding()
-						.background(Color.blue)
-						.cornerRadius(15)
-						.padding(.horizontal, 10)
-						.safeHover()
-				}
-				Spacer()
-				
-				NavigationLink(destination: properView(for: FSItem(path: parentDirectory(tmp_directory.path)))){
-					Text("App Container 💾")
-						.foregroundColor(.primary)
-						.bold()
-						.padding()
-						.background(Color.blue)
-						.cornerRadius(15)
-						.padding(.horizontal, 10)
-						.safeHover()
-				}
+					BookmarkItem(name: "Media 🖥", path: "/var/mobile/Media/")
+					BookmarkItem(name: "Documents 🗂", path: documents_directory)
+					BookmarkItem(name: "App Container 💾", path: parentDirectory(tmp_directory.path))
 				#endif
-				
-				Spacer()
 				
 				#warning("Should find a way to update deletion and adding of elements in realtime")
 				ForEach(userDefaultsKeys){ key in
-					FavoriteItem(key: key, type: .userAdded)
-					Spacer()
+					BookmarkItem(key: key)
 				}
 			}.padding(.horizontal)
 		}
 	}
 }
 
-enum FavoriteItemType{
-	case system, userAdded
-}
 
-struct FavoriteItem: View {
+#warning("Try colored background and primaryColor text on GoTo buttons")
+struct BookmarkItem: View {
+	
+	enum BookmarkItemType{
+		case system, userAdded
+	}
 	
 	var key : String
-	
-	var type : FavoriteItemType
+	var name : String
+	var path : String
+	var type : BookmarkItemType
 	
 	var color : Color{
 		switch self.type{
@@ -393,16 +338,23 @@ struct FavoriteItem: View {
 		}
 	}
 	
+	init(name: String, path: String){
+		self.key = ""
+		self.name = name
+		self.path = path
+		self.type = .system
+	}
+	
+	init(key: String){
+		self.key = key
+		self.name = String(key.split(separator: "_").last!)
+		self.path = String(userDefaults.string(forKey: key) ?? "/")
+		self.type = .userAdded
+	}
+	
 	var body: some View {
-		NavigationLink(destination:
-			properView(for: FSItem(path: userDefaults.string(forKey: self.key) ?? "/"))
-		){
-			Text(String(key.split(separator: "_").last!))
-				.foregroundColor(.primary)
-				.bold()
-				.padding()
-				.background(self.color)
-				.cornerRadius(15)
+		NavigationLink(destination: properView(for: FSItem(path: self.path))){
+			Text(name)
 				.contextMenu{
 					Button(action: {
 						userDefaults.removeObject(forKey: self.key)
@@ -410,16 +362,20 @@ struct FavoriteItem: View {
 						UIApplication.shared.shortcutItems?.removeAll(where: { shortcut in
 							return shortcut.type == self.key
 						})
-						
 					}){
 						Image(systemName: "bin.xmark.fill")
 						Text("Delete")
-					}.foregroundColor(.red)
-			}
+					}
+					.foregroundColor(.red)
+				}
+				.padding()
+				.foregroundColor(self.color)
+				.font(.system(size: 15).bold())
 		}
+		.buttonStyle(.bordered)
 		.padding(.horizontal, 10)
 		.safeHover()
-
+		Spacer()
 	}
 	
 }
@@ -427,7 +383,6 @@ struct FavoriteItem: View {
 extension View {
 	
 	// This is really ugly but it's the only way i could find to not require iOS 13.4 just for the hover function
-	
 	func safeHover() -> AnyView {
 		if #available(iOS 13.4, *){
 			return AnyView(hoverEffect(.lift))
