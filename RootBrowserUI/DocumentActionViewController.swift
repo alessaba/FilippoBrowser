38373f6ae86599a10b5366e4a610b9ddc595cb4a
alessaba <alessandrosaba023@gmail.com>
2021-10-09 14:35:59 +0200

    Tidy up UI Extension for future work

--- RootBrowserUI/DocumentActionViewController.swift
+++ RootBrowserUI/DocumentActionViewController.swift
@@ -10,23 +10,15 @@ import UIKit
 import FileProviderUI
 
 class DocumentActionViewController: FPUIActionExtensionViewController {
-    
-    @IBOutlet weak var identifierLabel: UILabel!
-    @IBOutlet weak var actionTypeLabel: UILabel!
-    
     override func prepare(forAction actionIdentifier: String, itemIdentifiers: [NSFileProviderItemIdentifier]) {
-        identifierLabel?.text = actionIdentifier
-        actionTypeLabel?.text = "Custom action"
+		#warning("Fetch path from lookupTable, then get the path and add the path to FilippoBrowser's Bookmarks")
+		
+		print("Completing task...")
+		//extensionContext.completeRequest()
     }
     
     override func prepare(forError error: Error) {
-        identifierLabel?.text = error.localizedDescription
-        actionTypeLabel?.text = "Authenticate"
-    }
-
-    @IBAction func doneButtonTapped(_ sender: Any) {
-        // Perform the action and call the completion block. If an unrecoverable error occurs you must still call the completion block with an error. Use the error code FPUIExtensionErrorCode.failed to signal the failure.
-        extensionContext.completeRequest()
+		print("Error \(error.localizedDescription)")
     }
     
     @IBAction func cancelButtonTapped(_ sender: Any) {
